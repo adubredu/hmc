1da43586d66575bb758f7e5eed43ef1c682face1
Ethan Goan <ethan@ethan-OptiPlex-7050>
2019-02-01 16:32:10 +1000

    included some comments directing me where to go

--- hmc_demo.py
+++ hmc_demo.py
@@ -1,12 +1,72 @@
+#!/usr/bin/env python
+""" Demo to try and get HMC working 
+
+Author: Ethan Goan
+        ej.goan@qut.edu.au
+Licence: Apache Licence 2.0 
+
+Notation: Following examples and similar notation
+to that in [1], which is probably not the best choice of
+variable names, but have chosen param names here to remain
+consistent with this.
+
+q = params = position (posterior params of interest)
+p = momentum/velocity
+z = (q, p)
+U(q) = Potential Energy
+K(p) = Kinetic Energy
+H(q, p) = Hamiltonian 
+        = Total Energy
+        = U(q) + K(p)
+  
+Derivatives: (assume partial derivatives)
+dq_i/dt = dH/dp_i
+dp_i/dt = - dH/dq_i
+dz/dt = J x gradient(H(z))
+where J = [ 0_{dxd}   I_{dxd} ]
+          [-I_{dxd}   0_{dxd} ]
+is a matrix and d is dimension of q
+
+  
+[1] Neal, Radford. "MCMC using Hamiltonian dynamics."
+    Handbook of Markov Chain Monte Carlo (2011).
+"""
+
+
 import os
 import sys
 
 import numpy as np
+from scipy.stats import multivariate_normal
 import tensorflow as tf
 import pandas as pd
 import matplotlib
 #matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+
+
+
+def grad_potential_gaussian(q, prior, likelihood):
+  """Computes gradient of potential energy for MVN
+
+  Potential energy for HMC is typically defined as,
+  U(q) = -log(p(q) p(x | q)) = -log(p(q)) - log(p(x | q))
+  And we will assume that we have a Gaussian Prior and likelihood.
+  From the Matrix Cookbook [1, equation 85], know that
+  d(log(p(x)))/dq = Sigma^{-1}(q - mu)
+  This is because we are using the log of the distribution, so we get rid
+  of the exp() and seperate the exponent from the normalising constant,
+  and when taking the derivative the constant terms will vanish, and
+  because a valid covariance matrix is symmetric.
+  
+  Args:
+    x (scipy.stats_multivariate_normal)
+      variable that we want to find the gradient of
+  """
+  
+  
+
+
 
 def one_d(n_samp = 20):
   """ simple 1-d example from Neal Sec 2.
@@ -52,41 +112,57 @@ def one_d(n_samp = 20):
     axs.tick_params(axis = 'both', width = 0.2)
   ax.set_ylabel('Momentum $(p)$')
   ax.set_xlabel('Position $(q)$', usetex=True)
-  plt.show()
-  
-  
-  
+  plt.show()  
   
 
 
-if __name__ == "__main__":
-  """ Demo to try and get HMC working
+def simple_gaussian_hmc(epsilon = 0.2, L = 10, iters = 100):
+  """Compare HMC for sample where solution is known
   
-  
-  Notation: Following similar notation to that in [1]
-  
-  q = params = position
-  p = momentum/velocity
-  z = (q, p)
-  U(q) = Potential Energy
-  K(p) = Kinetic Energy
-  H(q, p) = Hamiltonian 
-          = Total Energy
-          = U(q) + K(p)
-  
-  Derivatives: (assume partial derivatives)
-  dq_i/dt = dH/dp_i
-  dp_i/dt = - dH/dq_i
-  dz/dt = J x gradient(H(z))
-    where J = [ 0_{dxd}   I_{dxd} ]
-              [-I_{dxd}   0_{dxd} ]
-    is a matrix and d is dimension of q
-  
-  
-  [1] Neal, Radford. "MCMC using Hamiltonian dynamics."
-      Handbook of Markov Chain Monte Carlo 2 (2011).
+  Plot HMC draws from posterior against that of true posterior
+  to see how it all works.
+  Will work with a 2D Gaussian, and will look at posterior over the 
+  mean. Using same notation as above (and including new variables for
+  covariance etc.),
+  p(q) = N(q|0, I) = Prior 
+  p(x | q) = N(x| q, C)
+  p(q | x) = N(q| mu, Sigma) 
+  Will maybe type this out in TeX for full derivation
+
+  Args:
+    epsilon (float):
+      step-size in leapfrom algorithm
+    L (int):
+      Number of steps to take before updating
+    iters (int):
+      number of times to iterate through series of L steps
+      (L steps per iteration)
   """
-  
+  # initial value of position
+  current_q = np.zeros([0, 0])
+  # prior with independant components
+  q_prior = multivariate_normal([0. 0], mean = 0, cov = 1)
+  # momentum
+  p_dist = multivariate_normal([0. 0], mean = 0, cov = 1)
+  # where we will save the accepted values of position
+  q_acc = []
+  # lets get to it
+  for i in range(0, iters):
+    q = current_q
+    p = p_dis.rvs()
+    # save the current value of p
+    p_current = p
+    for j in range(0, L):
+
+      # half update of momentum
+      p = p - epsilon * 
+
+
+      
+      
+      
+      
+if __name__ == "__main__":
   # start with a simple 1-d example
   one_d()
   
